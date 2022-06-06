8220cfb68bf48193bf5646405d23dc9a342394fe
Pintu Das <rex@workwithme.in>
2022-06-06 21:42:51 +0530

    JSON output support added

--- index.js
+++ index.js
@@ -6,26 +6,25 @@ let currentPath = path.relative(process.cwd(), __dirname);
 if (!currentPath) currentPath = '.';
 
 async function fetchMediaInfoFromUrl(url) {
-  // ./cli/mediainfo ${url}
-  const { stdout, stderr } = await exec(`${currentPath}/cli/mediainfo ${url}`, {
+  const { stdout, stderr } = await exec(`${currentPath}/cli/mediainfo ${url} --output=JSON`, {
     cwd: process.cwd(),
   });
   if (stderr) {
     console.error('stderr:', stderr);
     return null;
   }
-  return stdout;
+  return JSON.parse(stdout);
 }
 
 async function fetchMediaInfoFromFile(filePath) {
   const { stdout, stderr } = await exec(
-    `${currentPath}/cli/mediainfo ${filePath}`
+    `${currentPath}/cli/mediainfo ${filePath} --output=JSON`,
   );
   if (stderr) {
     console.error('stderr:', stderr);
     return null;
   }
-  return stdout;
+  return JSON.parse(stdout);
 }
 
 module.exports = { fetchMediaInfoFromUrl, fetchMediaInfoFromFile };
